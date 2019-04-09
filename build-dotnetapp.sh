314d1327d02a254b61649a2707910f4c3fdd6d93
root <root@intranet>
2019-04-09 03:58:12 +0000

    Sample scripts for building apps from the official Docker .NET Core repo

--- build-dotnetapp.sh
+++ build-dotnetapp.sh
@@ -3,11 +3,10 @@
 if [ ! -d /samples ]; then
   mkdir samples
 fi
-if [ ! -d "samples/dotnet-docker" ]; then
+if [ ! -d "samples/dotnetapp" ]; then
   git clone https://github.com/dotnet/dotnet-docker samples/dotnet-docker
   mv samples/dotnet-docker/samples/* samples/ 
   rm -rf samples/dotnet-docker
 fi
-sudo docker run --rm -ti -v $PWD/samples/dotnetapp:/app indiewebconsult/dotnetcore-sdk dotnet restore
-
+sudo docker run --rm -ti -v $PWD/samples/dotnetapp:/app indiewebconsult/dotnetcore-sdk dotnet restore /app/dotnetapp
 sudo docker run --rm -ti -v $PWD/samples/dotnetapp:/app indiewebconsult/dotnetcore-sdk dotnet build /app/dotnetapp -o /app/build -c Release 
